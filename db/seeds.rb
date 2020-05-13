d5283e72a0dc42164e66e781eb9f9d387e8c0016
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-13 14:40:14 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating fake restaurants...'
+
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::TvShows::SouthPark.character,
+    address: Faker::TvShows::SouthPark.quote,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+end
+
+puts "Finished!"

2c00d13498fa304c3aa32cb4b4a144941fcd1ba9
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-13 11:54:53 +0200

    models added

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+
+  RATING = [0, 1, 2, 3, 4, 5]
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: RATING } 
+end
